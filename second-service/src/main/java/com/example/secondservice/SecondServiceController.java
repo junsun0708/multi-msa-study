da15f8c28274a8a2317735eb3ad8b97212afa107
JYH <salvayh@thingspire.com>
2023-09-07 18:24:05 +0900

    1. api-gateway

---
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,18 @@
+package com.example.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service")
+public class SecondServiceController {
+	
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+}
